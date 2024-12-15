c23ae36e527b99586e2a73aa7c3ee78bee00dd95
Jan Procházka <jan.prochy@gmail.com>
2024-12-15 03:57:00 +0100

    chore: angular 19 update done

--- projects/ngx-float-ui-repo/src/app/app.component.ts
+++ projects/ngx-float-ui-repo/src/app/app.component.ts
@@ -1,16 +1,14 @@
-import {Component, ViewEncapsulation} from "@angular/core";
+import { Component, ViewEncapsulation } from "@angular/core";
 //
-import {NGX_FLOAT_UI_ENVIRONMENT} from "../environments/environment";
-
+import { NGX_FLOAT_UI_ENVIRONMENT } from "../environments/environment";
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.scss"],
-    encapsulation: ViewEncapsulation.None
+    standalone: false,
+    encapsulation: ViewEncapsulation.None,
 })
 export class NgxFloatUiAppComponent {
-
     isDemo: boolean = NGX_FLOAT_UI_ENVIRONMENT.isDemo;
-
 }
--- projects/ngx-float-ui-repo/src/app/components/test/test.component.ts
+++ projects/ngx-float-ui-repo/src/app/components/test/test.component.ts
@@ -1,17 +1,19 @@
-import {Component} from "@angular/core";
+import { Component } from "@angular/core";
 //
-import {NgxFloatUiContentComponent, NgxFloatUiDirective, NgxFloatUiPlacements} from "ngx-float-ui";
+import {
+    NgxFloatUiContentComponent,
+    NgxFloatUiDirective,
+    NgxFloatUiPlacements,
+} from "ngx-float-ui";
 
 @Component({
     selector: "app-test",
     templateUrl: "test.component.html",
     styleUrls: ["test.component.scss"],
     standalone: true,
-    imports: [NgxFloatUiContentComponent, NgxFloatUiDirective]
+    imports: [NgxFloatUiContentComponent, NgxFloatUiDirective],
 })
 export class NgxFloatUiTestComponent {
-
     popperPlacements: typeof NgxFloatUiPlacements = NgxFloatUiPlacements;
     year: number = new Date().getFullYear();
-
 }
